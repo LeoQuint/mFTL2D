d7e21077440f9a7bfdc508baea816bba5fcc54d2
Leonard <leonardquintal@gmail.com>
2017-05-17 12:51:32 -0400

    setting up

---
+++ Assets/Scripts/ShipCam.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipCam : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void FixedUpdate () {
+		
+	}
+}
